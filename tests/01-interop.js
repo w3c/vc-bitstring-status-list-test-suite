92bfde23c60bd5eec9d91b2c11b0bb743077d900
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-02-15 09:23:59 -0500

    Move tests inside describe block.

--- tests/01-interop.js
+++ tests/01-interop.js
@@ -152,59 +152,32 @@ describe('StatusList2021 Credentials Test', function() {
         });
       }
       for(const verifier of testAPIs) {
-        it.skip('MUST verify a valid "StatusList2021Credential"',
-          async function() {
+        describe(verifier.name, function() {
+          it.skip('MUST verify a valid "StatusList2021Credential"',
+            async function() {
             // this tells the test report which cell
             // in the interop matrix the result goes in
-            this.test.cell = {
-              columnId: verifier.name,
-              rowId: this.test.title
-            };
-            const implementation = new Implementation(verifier);
-            let response;
-            let err;
-            try {
-              response = await implementation.verify({
-                credential: validVC
-              });
-            } catch(e) {
-              err = e;
-            }
-            should.exist(response);
-            should.not.exist(err);
-          });
-        it('MUST fail to verify a VC with invalid ' +
+              this.test.cell = {
+                columnId: verifier.name,
+                rowId: this.test.title
+              };
+              const implementation = new Implementation(verifier);
+              let response;
+              let err;
+              try {
+                response = await implementation.verify({
+                  credential: validVC
+                });
+              } catch(e) {
+                err = e;
+              }
+              should.exist(response);
+              should.not.exist(err);
+            });
+          it('MUST fail to verify a VC with invalid ' +
           '"credentialStatus.statusListCredential"', async function() {
           // this tells the test report which cell
           // in the interop matrix the result goes in
-          this.test.cell = {
-            columnId: verifier.name,
-            rowId: this.test.title
-          };
-          const implementation = new Implementation(verifier);
-          let response;
-          let err;
-          try {
-            response = await implementation.verify({
-              credential: invalidStatusListCredentialId
-            });
-          } catch(e) {
-            err = e;
-          }
-          should.not.exist(response);
-          should.exist(err);
-          should.exist(err.data);
-          // verifier returns 400
-          err.status.should.equal(400);
-          err.data.verified.should.equal(false);
-          const {check} = err.data.checks[0];
-          check.should.be.an('array');
-          check.should.include.members(['credentialStatus', 'proof']);
-        });
-        it('MUST fail to verify a VC with invalid "credentialStatus.type"',
-          async function() {
-            // this tells the test report which cell
-            // in the interop matrix the result goes in
             this.test.cell = {
               columnId: verifier.name,
               rowId: this.test.title
@@ -214,7 +187,7 @@ describe('StatusList2021 Credentials Test', function() {
             let err;
             try {
               response = await implementation.verify({
-                credential: invalidCredentialStatusType
+                credential: invalidStatusListCredentialId
               });
             } catch(e) {
               err = e;
@@ -225,7 +198,36 @@ describe('StatusList2021 Credentials Test', function() {
             // verifier returns 400
             err.status.should.equal(400);
             err.data.verified.should.equal(false);
+            const {check} = err.data.checks[0];
+            check.should.be.an('array');
+            check.should.include.members(['credentialStatus', 'proof']);
           });
+          it('MUST fail to verify a VC with invalid "credentialStatus.type"',
+            async function() {
+            // this tells the test report which cell
+            // in the interop matrix the result goes in
+              this.test.cell = {
+                columnId: verifier.name,
+                rowId: this.test.title
+              };
+              const implementation = new Implementation(verifier);
+              let response;
+              let err;
+              try {
+                response = await implementation.verify({
+                  credential: invalidCredentialStatusType
+                });
+              } catch(e) {
+                err = e;
+              }
+              should.not.exist(response);
+              should.exist(err);
+              should.exist(err.data);
+              // verifier returns 400
+              err.status.should.equal(400);
+              err.data.verified.should.equal(false);
+            });
+        });
       }
     });
   }
