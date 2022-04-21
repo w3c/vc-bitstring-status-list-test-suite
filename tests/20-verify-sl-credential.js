4aebc0fb319bfb680b564db89543e8cc12a5bc79
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-04-21 07:56:33 -0400

    Move `handler` to `vc-generator/documentLoader.js` & fix `Verify` tests.

--- tests/20-verify-sl-credential.js
+++ tests/20-verify-sl-credential.js
@@ -1,86 +1,107 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
 
-// const invalidCredentialStatusType =
-//   require('../static-vcs/invalidCredentialStatusType.json');
-// const invalidStatusListCredentialId =
-//   require('../static-vcs/invalidStatusListCredentialId.json');
-// const validVc = require('../static-vcs/validVc.json');
+const chai = require('chai');
+const {implementations} = require('vc-api-test-suite-implementations');
 
-// describe('StatusList2021 Credentials (verify)', function() {
-//   for(const [name, implementation] of implementations) {
-//     describe(name, function() {
-//       it.skip('MUST verify a valid "StatusList2021Credential"',
-//         async function() {
-//         // this tells the test report which cell
-//         // in the interop matrix the result goes in
-//           this.test.cell = {
-//             columnId: name,
-//             rowId: this.test.title
-//           };
-//           let response;
-//           let err;
-//           try {
-//             response = await implementation.verify({
-//               credential: validVc
-//             });
-//           } catch(e) {
-//             err = e;
-//           }
-//           should.exist(response);
-//           should.not.exist(err);
-//         });
-//       it('MUST fail to verify a VC with invalid ' +
-//       '"credentialStatus.statusListCredential"', async function() {
-//       // this tells the test report which cell
-//       // in the interop matrix the result goes in
-//         this.test.cell = {
-//           columnId: verifier.name,
-//           rowId: this.test.title
-//         };
-//         const implementation = new Implementation(verifier);
-//         let response;
-//         let err;
-//         try {
-//           response = await implementation.verify({
-//             credential: invalidStatusListCredentialId
-//           });
-//         } catch(e) {
-//           err = e;
-//         }
-//         should.not.exist(response);
-//         should.exist(err);
-//         should.exist(err.data);
-//         // verifier returns 400
-//         err.status.should.equal(400);
-//         err.data.verified.should.equal(false);
-//         const {check} = err.data.checks[0];
-//         check.should.be.an('array');
-//         check.should.include.members(['credentialStatus', 'proof']);
-//       });
-//       it('MUST fail to verify a VC with invalid "credentialStatus.type"',
-//         async function() {
-//         // this tells the test report which cell
-//         // in the interop matrix the result goes in
-//           this.test.cell = {
-//             columnId: verifier.name,
-//             rowId: this.test.title
-//           };
-//           const implementation = new Implementation(verifier);
-//           let response;
-//           let err;
-//           try {
-//             response = await implementation.verify({
-//               credential: invalidCredentialStatusType
-//             });
-//           } catch(e) {
-//             err = e;
-//           }
-//           should.not.exist(response);
-//           should.exist(err);
-//           should.exist(err.data);
-//           // verifier returns 400
-//           err.status.should.equal(400);
-//           err.data.verified.should.equal(false);
-//         });
-//     });
-//   }
-// });
+const invalidCredentialStatusType =
+  require('../static-vcs/invalidCredentialStatusType.json');
+const invalidStatusListCredentialId =
+  require('../static-vcs/invalidStatusListCredentialId.json');
+const validVc = require('../static-vcs/validVc.json');
+
+const should = chai.should();
+
+describe('StatusList2021 Credentials (Verify)', function() {
+  for(const [name, implementation] of implementations) {
+    const summaries = new Set();
+    this.summary = summaries;
+    // column names for the matrix go here
+    const columnNames = [];
+    const reportData = [];
+    // this will tell the report
+    // to make an interop matrix with this suite
+    this.matrix = true;
+    this.report = true;
+    this.columns = columnNames;
+    this.rowLabel = 'Test Name';
+    this.columnLabel = 'Verifier';
+    // the reportData will be displayed under the test title
+    this.reportData = reportData;
+    describe(name, function() {
+      it('MUST verify a valid "StatusList2021Credential"',
+        async function() {
+        // this tells the test report which cell
+        // in the interop matrix the result goes in
+          this.test.cell = {
+            columnId: name,
+            rowId: this.test.title
+          };
+          const verifier = implementation.verifiers.find(verifier =>
+            verifier.tags.has('VC-HTTP-API'));
+          const body = {
+            verifiableCredential: validVc,
+            options: {
+              checks: ['proof', 'credentialStatus']
+            }
+          };
+          const {result, error} = await verifier.verify({body});
+          should.exist(result);
+          should.not.exist(error);
+        });
+      it('MUST fail to verify a VC with invalid ' +
+      '"credentialStatus.statusListCredential"', async function() {
+      // this tells the test report which cell
+      // in the interop matrix the result goes in
+        this.test.cell = {
+          columnId: name,
+          rowId: this.test.title
+        };
+        const verifier = implementation.verifiers.find(verifier =>
+          verifier.tags.has('VC-HTTP-API'));
+        const body = {
+          verifiableCredential: invalidStatusListCredentialId,
+          options: {
+            checks: ['proof', 'credentialStatus']
+          }
+        };
+        const {result, error} = await verifier.verify({body});
+        should.not.exist(result);
+        should.exist(error);
+        should.exist(error.data);
+        // verifier returns 400
+        error.status.should.equal(400);
+        error.data.verified.should.equal(false);
+        const {check} = error.data.checks[0];
+        check.should.be.an('array');
+        check.should.include.members(['credentialStatus', 'proof']);
+      });
+      it('MUST fail to verify a VC with invalid "credentialStatus.type"',
+        async function() {
+        // this tells the test report which cell
+        // in the interop matrix the result goes in
+          this.test.cell = {
+            columnId: name,
+            rowId: this.test.title
+          };
+          const verifier = implementation.verifiers.find(verifier =>
+            verifier.tags.has('VC-HTTP-API'));
+          const body = {
+            verifiableCredential: invalidCredentialStatusType,
+            options: {
+              checks: ['proof', 'credentialStatus']
+            }
+          };
+          const {result, error} = await verifier.verify({body});
+          should.not.exist(result);
+          should.exist(error);
+          should.exist(error.data);
+          // verifier returns 400
+          error.status.should.equal(400);
+          error.data.verified.should.equal(false);
+        });
+    });
+  }
+});
--- vc-generator/documentLoader.js
+++ vc-generator/documentLoader.js
@@ -4,9 +4,30 @@
 */
 'use strict';
 
+const {httpClient} = require('@digitalbazaar/http-client');
+const https = require('https');
 const {JsonLdDocumentLoader} = require('jsonld-document-loader');
-const {contextMap} = require('./contexts');
+const {contextMap} = require('./contexts.js');
+
+const agent = new https.Agent({rejectUnauthorized: false});
+
+const handler = {
+  async get({url}) {
+    if(!url.startsWith('http')) {
+      throw new Error('NotFoundError');
+    }
+    let result;
+    try {
+      result = await httpClient.get(url, {agent});
+    } catch(e) {
+      throw new Error('NotFoundError');
+    }
+    return result.data;
+  }
+};
+
 const jdl = new JsonLdDocumentLoader();
+jdl.setProtocolHandler({protocol: 'https', handler});
 
 // add contexts to documentLoad
 for(const [key, value] of contextMap) {
