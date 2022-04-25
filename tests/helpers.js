cd1eadd7f668c7704f05f9fe9f7a93e6bc3ea28a
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-04-25 12:28:34 -0400

    Use `notImplemented` to populate tables.

--- tests/helpers.js
+++ tests/helpers.js
@@ -33,29 +33,7 @@ const getCredentialStatus = async ({verifiableCredential}) => {
   return {status, statusListCredential: revocationListCredential};
 };
 
-/**
- * Takes in a Map and a predicate and returns a new Map
- * only returning key value pairs that are true.
- *
- * @param {object} options - Options to use.
- * @param {Map} options.map - A Map.
- * @param {Function<boolean>} options.predicate - A function to
- * filter the map's entries on.
- *
- * @returns {Map} Returns a map.
- */
-const filterMap = ({map, predicate}) => {
-  const filtered = new Map();
-  for(const [key, value] of map) {
-    const result = predicate({key, value});
-    if(result === true) {
-      filtered.set(key, value);
-    }
-  }
-  return filtered;
-};
 module.exports = {
   ISOTimeStamp,
-  filterMap,
   getCredentialStatus
 };
