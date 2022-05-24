01ab891f702bc2833ca7d1c7ff845712b28d8848
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-05-24 13:41:45 -0400

    Add issuer tests for credential with suspension type statusPurpose.

--- tests/10-issue.js
+++ tests/10-issue.js
@@ -14,60 +14,116 @@ const should = chai.should();
 // only use implementations with `StatusList2021` issuers.
 const {match, nonMatch} = filterByTag({
   property: 'issuers',
-  tags: ['RevocationList2020']
+  tags: ['StatusList2021']
 });
-
-describe('StatusList2021 Credentials (Issue)', function() {
-  this.matrix = true;
-  this.report = true;
-  this.implemented = [...match.keys()];
-  this.rowLabel = 'Test Name';
-  this.columnLabel = 'Issuer';
-  this.notImplemented = [...nonMatch.keys()];
-  for(const [issuerName, {issuers}] of match) {
-    describe(issuerName, function() {
-      let issuerResponse;
-      let err;
-      let issuedVc;
-      before(async function() {
-        const issuer = issuers.find(
-          issuer => issuer.tags.has('StatusList2021'));
-        const {settings: {id: issuerId}} = issuer;
-        const credential = createValidVc({issuerId});
-        const body = {credential};
-        const {result, error, data} = await issuer.post({json: body});
-        err = error;
-        issuerResponse = result;
-        issuedVc = data;
-      });
-      it('MUST issue a VC with a "credentialStatus" property and ' +
-        '"credentialStatus.type" StatusList2021Entry', async function() {
-        this.test.cell = {columnId: issuerName, rowId: this.test.title};
-        should.exist(issuerResponse);
-        should.not.exist(err);
-        issuerResponse.status.should.equal(201);
-        should.exist(issuedVc, `Expected VC from ${issuerName} to exist.`);
-        testCredential({credential: issuedVc});
-      });
-      // ensure that issued StatusList Credential contain correct properties
-      it('MUST have correct properties when dereferencing' +
-          '"credentialStatus.statusListCredential"', async function() {
-        this.test.cell = {columnId: issuerName, rowId: this.test.title};
-        const {slc} = await getSlc({issuedVc});
-        testSlCredential({slCredential: slc});
-      });
-      it('MUST be 16kb in size when "encodedList" is decoded',
-        async function() {
-          this.test.cell = {columnId: issuerName, rowId: this.test.title};
-          const {slc: {credentialSubject}} = await getSlc({issuedVc});
-          const {encodedList} = credentialSubject;
-          // Uncompress encodedList
-          const decoded = await sl.decodeList({encodedList});
-          should.exist(decoded);
-          // decoded size should be 16kb
-          const decodedSize = (decoded.length / 8) / 1024;
-          decodedSize.should.equal(16);
+describe('StatusList2021 Credentials (Issue "statusPurpose: revocation")',
+  function() {
+    this.matrix = true;
+    this.report = true;
+    this.implemented = [...match.keys()];
+    this.rowLabel = 'Test Name';
+    this.columnLabel = 'Issuer';
+    this.notImplemented = [...nonMatch.keys()];
+    for(const [issuerName, {issuers}] of match) {
+      describe(issuerName, function() {
+        let issuerResponse;
+        let err;
+        let issuedVc;
+        before(async function() {
+          const issuer = issuers.find(
+            issuer => issuer.tags.has('Revocation'));
+          const {settings: {id: issuerId}} = issuer;
+          const credential = createValidVc({issuerId});
+          const body = {credential};
+          const {result, error, data} = await issuer.post({json: body});
+          err = error;
+          issuerResponse = result;
+          issuedVc = data;
         });
-    });
-  }
-});
+        it('MUST issue a VC with a "credentialStatus" property and ' +
+        '"credentialStatus.type" StatusList2021Entry', async function() {
+          this.test.cell = {columnId: issuerName, rowId: this.test.title};
+          should.exist(issuerResponse);
+          should.not.exist(err);
+          issuerResponse.status.should.equal(201);
+          should.exist(issuedVc, `Expected VC from ${issuerName} to exist.`);
+          testCredential({credential: issuedVc});
+        });
+        // ensure that issued StatusList Credential contain correct properties
+        it('MUST have correct properties when dereferencing' +
+          '"credentialStatus.statusListCredential"', async function() {
+          this.test.cell = {columnId: issuerName, rowId: this.test.title};
+          const {slc} = await getSlc({issuedVc});
+          testSlCredential({slCredential: slc});
+        });
+        it('MUST be 16kb in size when "encodedList" is decoded',
+          async function() {
+            this.test.cell = {columnId: issuerName, rowId: this.test.title};
+            const {slc: {credentialSubject}} = await getSlc({issuedVc});
+            const {encodedList} = credentialSubject;
+            // Uncompress encodedList
+            const decoded = await sl.decodeList({encodedList});
+            should.exist(decoded);
+            // decoded size should be 16kb
+            const decodedSize = (decoded.length / 8) / 1024;
+            decodedSize.should.equal(16);
+          });
+      });
+    }
+  });
+
+describe('StatusList2021 Credentials (Issue "statusPurpose: suspension")',
+  function() {
+    this.matrix = true;
+    this.report = true;
+    this.implemented = [...match.keys()];
+    this.rowLabel = 'Test Name';
+    this.columnLabel = 'Issuer';
+    this.notImplemented = [...nonMatch.keys()];
+    for(const [issuerName, {issuers}] of match) {
+      describe(issuerName, function() {
+        let issuerResponse;
+        let err;
+        let issuedVc;
+        before(async function() {
+          const issuer = issuers.find(
+            issuer => issuer.tags.has('Suspension'));
+          const {settings: {id: issuerId}} = issuer;
+          const credential = createValidVc({issuerId});
+          const body = {credential};
+          const {result, error, data} = await issuer.post({json: body});
+          err = error;
+          issuerResponse = result;
+          issuedVc = data;
+        });
+        it('MUST issue a VC with a "credentialStatus" property and ' +
+        '"credentialStatus.type" StatusList2021Entry', async function() {
+          this.test.cell = {columnId: issuerName, rowId: this.test.title};
+          should.exist(issuerResponse);
+          should.not.exist(err);
+          issuerResponse.status.should.equal(201);
+          should.exist(issuedVc, `Expected VC from ${issuerName} to exist.`);
+          testCredential({credential: issuedVc});
+        });
+        // ensure that issued StatusList Credential contain correct properties
+        it('MUST have correct properties when dereferencing' +
+          '"credentialStatus.statusListCredential"', async function() {
+          this.test.cell = {columnId: issuerName, rowId: this.test.title};
+          const {slc} = await getSlc({issuedVc});
+          testSlCredential({slCredential: slc});
+        });
+        it('MUST be 16kb in size when "encodedList" is decoded',
+          async function() {
+            this.test.cell = {columnId: issuerName, rowId: this.test.title};
+            const {slc: {credentialSubject}} = await getSlc({issuedVc});
+            const {encodedList} = credentialSubject;
+            // Uncompress encodedList
+            const decoded = await sl.decodeList({encodedList});
+            should.exist(decoded);
+            // decoded size should be 16kb
+            const decodedSize = (decoded.length / 8) / 1024;
+            decodedSize.should.equal(16);
+          });
+      });
+    }
+  });
