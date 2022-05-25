5d90302627a680a12373bb5d6b7c578d3f2d2d25
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-05-25 10:24:56 -0400

    Update vc-generator.

--- vc-generator/index.js
+++ vc-generator/index.js
@@ -13,18 +13,26 @@ const credentialsPath = join(process.cwd(), 'static-vcs');
 
 // this will generate the signed VCs for the verifier tests
 const main = async () => {
-  const {path, validVc} = await _validVc();
+  const {
+    path1,
+    path2,
+    validVcForRevocation,
+    validVcForSuspension
+  } = await _validVcs();
+
   const vcs = [
-    _invalidCredentialStatusType(validVc),
-    _invalidStatusListCredentialId(validVc),
+    _invalidCredentialStatusType(validVcForRevocation),
+    _invalidStatusListCredentialId(validVcForRevocation),
   ];
   console.log('writing vcs to /credentials');
 
   // write them to disk
   await Promise.all([
     ...vcs,
-    // add the valid vc to the list
-    {path, data: validVc}
+    // add the valid vc for revocation to the list
+    {path: path1, data: validVcForRevocation},
+    // add the valid vc for revocation to the list
+    {path: path2, data: validVcForSuspension}
   ].map(writeJSON));
   console.log('vcs generated');
 
@@ -46,20 +54,29 @@ const main = async () => {
     };
   }
 
-  async function _validVc() {
+  async function _validVcs() {
     // get a VC issued by DB
     const {match} = filterImplementations({filter: ({value}) => {
       return value.settings.name === 'Digital Bazaar';
     }});
     const {issuers} = match.get('Digital Bazaar');
-    const issuer = issuers.find(
-      issuer => issuer.tags.has('StatusList2021'));
-    const {issuer: {id: issuerId}} = issuer;
-    const credential = createValidVc({issuerId});
-    const body = {credential};
-    const {data: validVc} = await issuer.issue({body});
-    return {path: `${credentialsPath}/validVc.json`, validVc};
+    const issuer1 = issuers.find(
+      issuer => issuer.tags.has('Revocation'));
+    const issuer2 = issuers.find(
+      issuer => issuer.tags.has('Suspension'));
+    const credential1 = createValidVc({issuer: issuer1});
+    const credential2 = createValidVc({issuer: issuer2});
+    const {data: validVcForRevocation} = await issuer1.issue(
+      {body: {credential: credential1}});
+    const {data: validVcForSuspension} = await issuer2.issue(
+      {body: {credential: credential2}});
+    return {
+      path1: `${credentialsPath}/validVcForRevocation.json`,
+      validVcForRevocation,
+      path2: `${credentialsPath}/validVcForSuspension.json`,
+      validVcForSuspension
+    };
   }
 };
-// run main by calling node ./vc-generator
+
 main();
