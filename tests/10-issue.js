970e5821b2356b2b5e5d606aa09d53fcd18c6df8
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-06-07 19:01:23 -0400

    Change back tag to `Revocation`.

--- tests/10-issue.js
+++ tests/10-issue.js
@@ -30,8 +30,7 @@ describe('StatusList2021 Credentials (Issue "statusPurpose: revocation")',
         let err;
         let issuedVc;
         before(async function() {
-          const issuer = issuers.find(issuer =>
-            issuer.tags.has('StatusList2021'));
+          const issuer = issuers.find(issuer => issuer.tags.has('Revocation'));
           const credential = createValidVc({issuer});
           const {result, error, data} = await issuer.post({
             json: {credential}});
